54ee6d62115bb1d7fa0414633cd7baf520eaf6e4
Steve Vaughan Jr <s_vaughan@apple.com>
2024-03-17 22:07:26 -0400

    Again

--- dev/install-comet-spark.sh
+++ dev/install-comet-spark.sh
@@ -5,7 +5,7 @@ set -e
 SCRIPT_NAME=$(echo $0 | sed 's/.*\///')
 
 function usage() {
-  echo "Usage: $SCRIPT_NAME [--use-existing-boson <version>] [<spark-branch>]"
+  echo "Usage: $SCRIPT_NAME [--use-existing-comet <version>] [<spark-branch>]"
   echo "       $SCRIPT_NAME -h|--help"
 
 }
@@ -22,18 +22,18 @@ die() {
 }
 
 BASEDIR=$(readlink -f "$(dirname $0)")
-BOSON_VERSION=""
-BOSON_WORKSPACE=$(cd $BASEDIR/..; pwd)
+COMET_VERSION=""
+COMET_WORKSPACE=$(cd $BASEDIR/..; pwd)
 
-SKIP_BOSON=false
+SKIP_COMET=false
 while [[ $# -gt 0 ]]; do
   case $1 in
     -h|--help)
       usageAndExit
       ;;
-    --use-existing-boson)
-      SKIP_BOSON=true
-      BOSON_VERSION=$2
+    --use-existing-comet)
+      SKIP_COMET=true
+      COMET_VERSION=$2
       shift 2
       ;;
     -*)
@@ -45,17 +45,17 @@ while [[ $# -gt 0 ]]; do
   esac
 done
 
-if [ -z "$BOSON_VERSION" ]; then
-  BOSON_VERSION=$($BOSON_WORKSPACE/mvnw -nsu -q $PROFILES help:evaluate -Dexpression=project.version -DforceStdout 2>/dev/null)
+if [ -z "$COMET_VERSION" ]; then
+  COMET_VERSION=$($COMET_WORKSPACE/mvnw -nsu -q $PROFILES help:evaluate -Dexpression=project.version -DforceStdout 2>/dev/null)
 fi
 SPARK_VERSION=$(./mvnw -nsu -q $PROFILES help:evaluate -Dexpression=spark.version -DforceStdout)
 SPARK_PATCH_VERSION=$(echo $SPARK_VERSION | sed -E 's/([0-9]+\.[0-9]+\.[0-9]+).*/\1/')
 SPARK_BRANCH=${1:-"branch-${SPARK_PATCH_VERSION}-apple"}
-SCALA_BINARY_VERSION=$($BOSON_WORKSPACE/mvnw -nsu -q $PROFILES help:evaluate -Dexpression=scala.binary.version -DforceStdout 2>/dev/null)
-SCALA_VERSION=$($BOSON_WORKSPACE/mvnw -nsu -q $PROFILES help:evaluate -Dexpression=scala.version -DforceStdout 2>/dev/null)
+SCALA_BINARY_VERSION=$($COMET_WORKSPACE/mvnw -nsu -q $PROFILES help:evaluate -Dexpression=scala.binary.version -DforceStdout 2>/dev/null)
+SCALA_VERSION=$($COMET_WORKSPACE/mvnw -nsu -q $PROFILES help:evaluate -Dexpression=scala.version -DforceStdout 2>/dev/null)
 
-if [ $SKIP_BOSON = "true" ]; then
-  echo Skipping boson build...
+if [ $SKIP_COMET = "true" ]; then
+  echo Skipping comet build...
 else
   make clean release
 fi
@@ -68,7 +68,7 @@ rm -rf /root/.m2/repository/org/apache/parquet
 # Removing local cached protobuf that also causes building Spark with SBT
 rm -rf /root/.m2/repository/com/google/protobuf
 
-cd $BOSON_WORKSPACE
+cd $COMET_WORKSPACE
 rm -rf apache-spark
 git clone git@github.pie.apple.com:IPR/apache-spark.git --depth 1 --branch $SPARK_BRANCH
 cd apache-spark
@@ -79,7 +79,7 @@ if [ -f "$BASEDIR/diff/$SPARK_BRANCH.diff" ]; then
 fi
 
 # Update the Boson version
-$BOSON_WORKSPACE/mvnw -nsu -q versions:set-property -Dproperty=boson.version  -DnewVersion=$BOSON_VERSION -DgenerateBackupPoms=false
+$COMET_WORKSPACE/mvnw -nsu -q versions:set-property -Dproperty=comet.version  -DnewVersion=$COMET_VERSION -DgenerateBackupPoms=false
 
 # Update the Scala version
 dev/change-scala-version.sh $SCALA_BINARY_VERSION
@@ -91,10 +91,10 @@ sed -i.bak -E '0,/javax.servlet/{s/^( *)(ExclusionRule\("javax.servlet")/\1\/\/\
 find resource-managers/kubernetes -name Dockerfile | xargs -n 1 sed -i.bak 's|^FROM |FROM docker-upstream.apple.com/|'
 
 # Store the Boson parameters
-cat > boson-parameters.sh <<EOD
-export BOSON_WORKSPACE=$BOSON_WORKSPACE
-export BOSON_VERSION=$BOSON_VERSION
+cat > comet-parameters.sh <<EOD
+export COMET_WORKSPACE=$COMET_WORKSPACE
+export COMET_VERSION=$COMET_VERSION
 export SCALA_VERSION=$SCALA_VERSION
 export SCALA_BINARY_VERSION=$SCALA_BINARY_VERSION
 EOD
-chmod a+x boson-parameters.sh
+chmod a+x comet-parameters.sh
